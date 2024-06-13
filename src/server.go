f213cb0b1b413c27c07fcf0fa18ab10b6e869845
tanner.caffrey <tanner.caffrey@gmail.com>
2024-06-13 12:50:56 -0400

    CodeForAnalyzing

--- src/server.go
+++ src/server.go
@@ -10,13 +10,43 @@ import (
 )
 
 // Define a struct for the JSON response
-type Response struct {
+type ServerResponse struct {
 	Message string `json:"message"`
+}
+
+type MediaMetadata struct {
+	// CreationTime string `json:"creationTime"`
+	Height string `json:"height"`
+	// Photo        Photo  `json:"photo"`
+	Width string `json:"width"`
+}
+
+// type Photo struct {
+// 	ApertureFNumber float64 `json:"apertureFNumber"`
+// 	CameraMake      string  `json:"cameraMake"`
+// 	CameraModel     string  `json:"cameraModel"`
+// 	ExposureTime    string  `json:"exposureTime"`
+// 	FocalLength     float64 `json:"focalLength"`
+// 	IsoEquivalent   int     `json:"isoEquivalent"`
+// }
+
+type MediaItem struct {
+	BaseUrl       string        `json:"baseUrl"`
+	Filename      string        `json:"filename"`
+	ID            string        `json:"id"`
+	MediaMetadata MediaMetadata `json:"mediaMetadata"`
+	// MimeType      string        `json:"mimeType"`
+	// ProductUrl    string        `json:"productUrl"`
+}
+
+type Response struct {
+	MediaItems    []MediaItem `json:"mediaItems"`
+	NextPageToken string      `json:"nextPageToken"`
 }
 
 // Handler function for the GET endpoint
 func photosHandler(w http.ResponseWriter, r *http.Request) {
-	response := Response{Message: "Hello, World!"}
+	response := ServerResponse{Message: "Hello, World!"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
@@ -69,7 +99,7 @@ func queryPhotos(client *http.Client) {
 		log.Fatalf("Unable to read response body: %v", err)
 	}
 
-	var result map[string]interface{}
+	var result Response
 	if err := json.Unmarshal(body, &result); err != nil {
 		log.Fatalf("Unable to decode response: %v", err)
 	}
